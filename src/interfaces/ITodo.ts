ca1e06a9d2fe25e06591b755ef1c30c1e5348842
rathdaro <rathdaro.kuy@hotmail.com>
2020-08-07 06:32:16 +0700

    Add some files

--- src/interfaces/ITodo.ts
+++ src/interfaces/ITodo.ts
@@ -1,6 +1,6 @@
 export interface ITodo {
     _id: string;
     title: string;
-    description: string;
+    info: string;
     complete: boolean;
 }
---
+++ src/subscribers/user.ts
@@ -0,0 +1,36 @@
+import { Container } from 'typedi';
+import { EventSubscriber, On, EventSubscriber } from 'event-dispatch';
+import events from './events';
+import { IUser } from '../interfaces/IUser';
+import mongoose from 'mongoose';
+
+@EventSubscriber()
+export default class UserSubscriber {
+    @On(events.user.signIn)
+    public onUserSignIn({ _id }: Partial<IUser>) {
+        const Logger = Container.get('logger');
+
+        try {
+            const UserModel = Container.get('UserModel') as mongoose.Model<IUser & mongoose.Document>;
+
+            UserModel.update({ _id }, { $set: { lastLogin: new Date() }});
+        } catch (e) {
+            Logger.error(`Error on event ${events.user.signIn}: %o`, e);
+            throw e;
+        }
+    }
+
+    @On(events.user.signUp)
+    public onUserSignUp({ name, email, _id }: Partial<IUser>) {
+        const Logger = Container.get('logger');
+
+        try {
+            const UserModel = Container.get('UserModel') as mongoose.Model<IUser & mongoose.Document>;
+            
+            UserModel.create({ });
+        } catch (e) {
+            Logger.error(`Error on event ${events.user.signUp}: %o`, e);
+            throw e;
+        }
+    }
+}
